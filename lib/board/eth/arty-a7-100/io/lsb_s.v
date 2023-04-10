22f25bb74cbdf669489cec980b2eff53c7ef512a
gray <gray@grayraven.org>
2023-04-10 16:46:13 +0400

    Add 7-segment display output for DE2-115.

--- lib/board/eth/arty-a7-100/io/lsb_s.v
+++ lib/board/eth/arty-a7-100/io/lsb_s.v
@@ -43,13 +43,16 @@ module lsb_s (
   wire wr_data = stb & we;
   wire rd_data = stb & ~we;
 
-  wire leds_r_on = data_in[31];
+  // control data in data_in
+  // for red LEDs and 7-segments
+  wire [1:0] ctrl = data_in[31:30];
 
-  reg [3:0] leds_g_s;
-  reg [3:0] leds_g_d;
-  assign leds_g[3:0] = leds_g_s[3:0] | leds_g_d[3:0];
+  // green LEDs controls
+  // OR-ed, see below
+  reg [3:0] leds_g_s; // direct hw signal
+  reg [3:0] leds_g_d; // via data
 
-  // debouncers
+  // debouncers-syncers
   dbnc #(.polarity(1)) dbnc_btn0 (.clk(clk), .btn_in(btn_in[0]), .btn_out(btn_out[0]));
   dbnc #(.polarity(1)) dbnc_btn1 (.clk(clk), .btn_in(btn_in[1]), .btn_out(btn_out[1]));
   dbnc #(.polarity(1)) dbnc_btn2 (.clk(clk), .btn_in(btn_in[2]), .btn_out(btn_out[2]));
@@ -60,11 +63,6 @@ module lsb_s (
   dbnc #(.polarity(1)) dbnc_swi2 (.clk(clk), .btn_in(swi_in[2]), .btn_out(swi_out[2]));
   dbnc #(.polarity(1)) dbnc_swi3 (.clk(clk), .btn_in(swi_in[3]), .btn_out(swi_out[3]));
 
-  // always @(posedge clk) begin
-  //   leds_sys <= rst ? 8'b0 : wr_data ? data_in[7:0] : leds;
-  //   led_g <= rst ? 4'b0 : led_g_in[3:0];
-  // end
-
   always @(posedge clk) begin
     if (rst) begin
       leds_sys[7:0] <= 8'b0;
@@ -72,18 +70,19 @@ module lsb_s (
       leds_g_d[3:0] <= 4'b0;
     end
     else begin
+      leds_g_s[3:0] <= leds_g_in[3:0];
       if (wr_data) begin
         leds_sys <= data_in[7:0];
-        if (leds_r_on) begin
-          leds_g_d[3:0] <= leds_g_d[3:0] | data_in[11:8];
-        end
-        else begin
-          leds_g_d[3:0] <= leds_g_d[3:0] & ~data_in[11:8];
-        end
+        case (ctrl[1:0])
+          2'b01: leds_g_d[3:0] <= leds_g_d[3:0] & ~data_in[11:8];   // off
+          2'b10: leds_g_d[3:0] <= leds_g_d[3:0] | data_in[11:8];    // off
+        endcase
       end
     end
-    leds_g_s[3:0] <= leds_g_in[3:0];
   end
+
+  // output assignments
+  assign leds_g[3:0] = leds_g_s[3:0] | leds_g_d[3:0];
 
   assign data_out[31:0] =
     rd_data ? {16'b0, 4'b0, btn_out[3:0], 4'b0, swi_out[3:0]} :
--- lib/board/thm/de2-115/io/lsb_s.v
+++ lib/board/thm/de2-115/io/lsb_s.v
@@ -1,28 +1,25 @@
 /**
   LSB: LEDs, Switches, Buttons
+  Generously, we subsume the 7-segment displays as LEDs. :)
   --
-  Architecture: AMY
+  Architecture: ANY
   Board: DE2-115
   --
   * Red LEDs are also switched by hardware input signals.
   * Red LEDs use "toggle" mechanism for on/off
+  * 7-segment displays (2 + 2 + 4, from left to right)
+  * All buttons handled here, incl. reset
   --
-  Based on THM bio.v
-  --
-  Seven segment display not accessible yet
+  Base/origin: THM
   --
   data_in [31:0]:
     [7:0]: green LEDs (8 of 9 total)
-    [25:8]: greed LEDs (18)
-    [30:26]: unused
-    [31:31]: green LEDs toggle
+    [25:8]: red LEDs (18), or 7-segment display value
+    [29:26]: 7-segment display selector (data as data_in[11:8])
+    [31:30]: red LEDs toggle (data as data_in[25:8])
   --
   2023 Gray, gray@grayraven.org
   https://oberon-rts.org/licences
-  * all buttons handled here, incl. reset
-  * change output data to align the "higher" switches with bit 16
-  * enable red LEDs
-  * add output wires for buttons and switches
 **/
 
 `timescale 1ns / 1ps
@@ -35,7 +32,7 @@ module lsb_s (
   input stb,
   input we,
   input [31:0] data_in,
-  input [17:0] leds_r_in,
+  input [17:0] leds_r_in,   // can only be clock-synchronised source
   output [31:0] data_out,
   output ack,
   // external interface,
@@ -58,16 +55,29 @@ module lsb_s (
   wire wr_data = stb & we;
   wire rd_data = stb & ~we;
 
-  wire leds_r_on = data_in[31];
+  // control data in data_in
+  // for red LEDs and 7-segments
+  wire [5:0] ctrl = data_in[31:26];
 
+  // 7-segment encoder out
+  wire [6:0] segs_n;
+
+  // clock synchronisers
   reg [3:0] btn_p_n;
   reg [3:0] btn_s_n;
   reg [17:0] swi_p;
   reg [17:0] swi_s;
 
-  reg [17:0] leds_r_s;
-  reg [17:0] leds_r_d;
-  assign leds_r[17:0] = leds_r_s[17:0] | leds_r_d[17:0];
+  // red LEDs controls
+  // OR-ed, see below
+  reg [17:0] leds_r_s;  // direct hw signal
+  reg [17:0] leds_r_d;  // via data
+
+  // 7-segment LUT
+  lut7 lut7_0 (
+    .digit(data_in[11:8]),
+    .segs_n(segs_n[6:0])
+  );
 
   always @(posedge clk) begin
     if (rst) begin
@@ -82,20 +92,28 @@ module lsb_s (
       hex2_n[6:0] <= ~7'h0;
       hex1_n[6:0] <= ~7'h0;
       hex0_n[6:0] <= ~7'h0;
-    end else begin
+    end
+    else begin
+      leds_r_s[17:0] <= leds_r_in[17:0];
       if (wr_data) begin
         leds_g[7:0] <= data_in[7:0];
-        if (leds_r_on) begin
-          leds_r_d[17:0] <= leds_r_d[17:0] | data_in[25:8];
-        end
-        else begin
-          leds_r_d[17:0] <= leds_r_d[17:0] & ~data_in[25:8];
-        end
+        case (ctrl[5:0])
+          6'b010000: leds_r_d[17:0] <= leds_r_d[17:0] & ~data_in[25:8];   // off
+          6'b100000: leds_r_d[17:0] <= leds_r_d[17:0] | data_in[25:8];    // on
+          6'b001000: hex0_n[6:0] <= segs_n[6:0];
+          6'b001001: hex1_n[6:0] <= segs_n[6:0];
+          6'b001010: hex2_n[6:0] <= segs_n[6:0];
+          6'b001011: hex3_n[6:0] <= segs_n[6:0];
+          6'b001100: hex4_n[6:0] <= segs_n[6:0];
+          6'b001101: hex5_n[6:0] <= segs_n[6:0];
+          6'b001110: hex6_n[6:0] <= segs_n[6:0];
+          6'b001111: hex7_n[6:0] <= segs_n[6:0];
+        endcase
       end
-      leds_r_s[17:0] <= leds_r_in[17:0];
     end
   end
 
+  // sync buttons and switches
   always @(posedge clk) begin
     btn_p_n[3:0] <= btn_in_n[3:0];
     btn_s_n[3:0] <= btn_p_n[3:0];
@@ -103,13 +121,47 @@ module lsb_s (
     swi_s[17:0] <= swi_p[17:0];
   end
 
+  // output assignments
   assign btn_out[3:0] = ~btn_s_n[3:0];
   assign swi_out[17:0] = swi_s[17:0];
 
+  assign leds_r[17:0] = leds_r_s[17:0] | leds_r_d[17:0];
+
   assign data_out[31:0] =
-    rd_data ? {6'b0, swi_out[17:8], 4'b0, btn_out[3:0], swi[7:0]} :
+    rd_data ? {6'b0, swi_out[17:8], 4'b0, btn_out[3:0], swi_out[7:0]} :
     32'b0;
 
   assign ack = stb;
 
 endmodule
+
+module lut7 (
+  input wire [3:0] digit,
+  output reg [6:0] segs_n
+);
+
+  always @(digit) begin
+    case (digit)
+      0: segs_n[6:0] = ~7'b0111111;
+      1: segs_n[6:0] = ~7'b0000110;
+      2: segs_n[6:0] = ~7'b1011011;
+      3: segs_n[6:0] = ~7'b1001111;
+      4: segs_n[6:0] = ~7'b1100110;
+      5: segs_n[6:0] = ~7'b1101101;
+      6: segs_n[6:0] = ~7'b1111101;
+      7: segs_n[6:0] = ~7'b0000111;
+      8: segs_n[6:0] = ~7'b1111111;
+      9: segs_n[6:0] = ~7'b1101111;
+      10: segs_n[6:0] = ~7'b1110111;  // A
+      11: segs_n[6:0] = ~7'b1111100;  // b
+      12: segs_n[6:0] = ~7'b0111001;  // C
+      13: segs_n[6:0] = ~7'b1011110;  // d
+      14: segs_n[6:0] = ~7'b1111001;  // E
+      15: segs_n[6:0] = ~7'b1110001;  // F
+      default: segs_n[6:0] = ~7'b0000000;
+    endcase
+end
+
+endmodule
+
+`resetall
